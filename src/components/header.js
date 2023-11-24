822b1ec87c6b26537a6900c79904c223bbf2503e
jennifersvensson1 <jennifer.svenssn@gmail.com>
2023-11-24 15:10:40 +0100

    Adds header nav

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Header.module.css'
+
+export default function Header() {
+    return (
+        <div className={styles.nav}>
+            <a href="#home">Landing</a>
+            <a href="#about">About me</a>
+            <a href="#projects">Projects</a>
+            <a href="#contact">Contact</a>
+        </div>
+    );   
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,10 @@
+import styles from '../styles/layout.module.css'
 import Header from './header';
 
 export default function Layout({ children }) {
     return (
         <div className={styles.container}>
-            {/* <Header /> */}
+            <Header />
             {children}
         </div>
     );
