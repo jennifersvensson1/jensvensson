38c7375a6c8e67a1afa466b6797a569161529440
jennifersvensson1 <jennifer.svenssn@gmail.com>
2023-11-24 17:08:34 +0100

    Adds project data

---
+++ src/data/projects.js
@@ -0,0 +1,33 @@
+export const projects = [
+    {
+        name: "Moodsic",
+        src: "moodsic.png",
+        desc: "A playlist generator based on desired mood and individual listening data.",
+        tags: ["Programming", "React"]
+    },
+    {
+        name: "Complementary Colors",
+        src: "compcolors.png",
+        desc: "Website that generates two complementary colors on each press of the 'space' button.",
+        tags: ["Programming"]
+    },
+    {
+        name: "Fresh Choices",
+        src: "freshchoices.png",
+        desc: "Game developed in Construct 3 for prototyping purposes.",
+        tags: ["Prototyping", "Game development"]
+    },
+    {
+        name: "AstroJump",
+        src: "astrojump.png",
+        desc: "",
+        tags: ["Programming", "Game development"]
+    },
+    {
+        name: "O3S",
+        src: "o3s/package.png",
+        desc: "",
+        tags: ["Graphic design"],
+        tools: ["Adobe"]
+    },
+]
