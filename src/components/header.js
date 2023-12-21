09a3c51aedf8148d7d75c2c2fcdab44c60c04abf
jennifersvensson1 <jennifer.svenssn@gmail.com>
2023-12-21 11:04:17 +0100

    Fixes header routing

--- src/components/header.js
+++ src/components/header.js
@@ -3,11 +3,11 @@ import styles from '../styles/Header.module.css'
 export default function Header() {
     return (
         <div className={styles.nav}>
-            <a href="#home">Landing</a>
-            <a href="#about">About me</a>
-            <a href="#tools">Tools</a>
-            <a href="#projects">Projects</a>
-            <a href="#contact">Contact</a>
+            <a href="../#home">Landing</a>
+            <a href="../#about">About me</a>
+            <a href="../#tools">Tools</a>
+            <a href="../#projects">Projects</a>
+            <a href="../#contact">Contact</a>
         </div>
     );   
 }
