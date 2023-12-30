a1791fc62d6d04bf969b2d05ca3da04eebeb97c7
jennifersvensson1 <jennifer.svenssn@gmail.com>
2023-12-30 19:39:01 +0100

    Adds specifics to project page

--- src/pages/projects/[slug].js
+++ src/pages/projects/[slug].js
@@ -1,10 +1,15 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 import { projects } from '../../data/projects';
 import styles from '../../styles/Home.module.css'
+import { tools } from '../../data/tools';
 
 import PhotoAlbum from "react-photo-album";
+
+import { FaGithub } from 'react-icons/fa';
+import { MdArrowOutward } from 'react-icons/md';
 
 export default function Project() {
     const router = useRouter();
@@ -26,11 +31,60 @@ export default function Project() {
                 <title>Jennifer's Portfolio: {project.title}</title>
             </Head>
             <main className={styles.main}>
-                <div className={styles.section}>
+                <div className={`${styles.section} ${styles.specSection}`}>
                     <h2>{project.title}</h2>
                     <h4>{project.subtitle}</h4>
                     <p>{project.desc}</p>
-                    <PhotoAlbum layout="rows" photos={project.images} />
+
+                    <div className={styles.projectSpec}>
+                        <div>
+                            <div className={styles.projectTags}>
+                                {project.tags.map((tag) =>
+                                    <p>#{tag}</p>
+                                )}
+                            </div>
+                            <div className={styles.projectTools}>
+                                {project.tools.map((tool, index) =>
+                                    <div key={tool + " " + index + 1} className={styles.toolIcon}>
+                                        {tools.find((t) => t.key == tool).icon}
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+
+                        <div className={styles.projectButtons}>
+                            {project.github ?
+                                <Link href={project.github}>
+                                    <a className={styles.githubButton} target="_blank">
+                                        <FaGithub className={styles.icon} />
+                                    </a>
+                                </Link>
+                                : ""
+                            }
+                            {project.demo ?
+                                <Link href={project.demo}>
+                                    <a className={styles.projectButton} target="_blank">
+                                        <p>Live Demo</p>
+                                        <MdArrowOutward className={styles.miniIcon} />
+                                    </a>
+                                </Link>
+                                : ""
+                            }
+                            {project.instructions ?
+                                <Link href={project.instructions}>
+                                    <a className={styles.projectButton} target="_blank">
+                                        <p>Instructions</p>
+                                        <MdArrowOutward className={styles.miniIcon} />
+                                    </a>
+                                </Link>
+                                : ""
+                            }
+                        </div>
+                    </div>
+                    <div className={styles.imagesGrid}>
+                        <PhotoAlbum layout="rows" photos={project.images} />
+                    </div>
+
                 </div>
             </main>
         </div>
