ac91846a6ef56cbb11582bc152a9c29ceb5531b4
jennifersvensson1 <jennifer.svenssn@gmail.com>
2023-12-30 16:21:20 +0100

    Adds projects info (+ florilegium)

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,8 +3,11 @@ export const projects = [
         name: "moodsic",
         title: "Moodsic",
         subtitle: "A playlist generator based on desired mood and individual listening data.",
-        desc: "",
-        tags: ["Programming"],
+        desc: "Moodsic is the result of my master thesis, my little baby that made the project possible. ",
+        tags: ["Web development"],
+        tools: ['react', 'nodejs', 'restfulapis', 'git', 'rwd', 'vscode'],
+        demo: "moodsic.herokuapp.com",
+        github: "https://github.com/jennifersvensson1/moodsic",
         images: [
             {
                 src: "/projects/moodsic/moodsic_1.png",
@@ -77,14 +80,16 @@ export const projects = [
                 height: 13
             },
         ],
-        color: "#000",
     },
     {
         name: "compcolors",
         title: "Complementary Colors",
         subtitle: "Generates two random complementary colors and their color codes.",
         desc: "With just one press of the space button, a random color and its complement are generated, where both the color name and its color codes are presented.",
-        tags: ["Programming"],
+        tags: ["Web development"],
+        tools: ["html5", "css3", "js", "restfulapis", "git", 'vscode'],
+        demo: "",
+        github: "https://github.com/jennifersvensson1/complementary-colors",
         images: [
             {
                 src: "/projects/compcolors/compcolors_1.png",
@@ -116,9 +121,12 @@ export const projects = [
     {
         name: "freshchoices",
         title: "Fresh Choices",
-        subtitle: "A game designed to encourage seasonal grocery shopping, developed in Construct 3 for prototyping purposes. ",
+        subtitle: "A game designed to encourage seasonal grocery shopping.",
         desc: "A prototype created in Construct 3 for a serious game where the goal is to buy food according to the season. For each season the player progresses through, they are presented with a list of seasonal ingredients and food options to purchase. What they decide to add to their shopping cart is up to them. The player's actions impact the planet's health, allowing them to see direct consequences of their own food choices.",
         tags: ["Prototyping", "Game development"],
+        tools: ["construct3"],
+        demo: "https://freshchoices.netlify.app/",
+        instructions: "https://jennifersvensson1.itch.io/freshchoices",
         images: [
             {
                 src: "/projects/freshchoices/freshchoices_1.png",
@@ -147,27 +155,27 @@ export const projects = [
               }
         ]
     },
-    {
-        name: "astrojump",
-        title: "AstroJump",
-        subtitle: "",
-        desc: "",
-        tags: ["Programming", "Game development"],
-        images: [
-            {
-                src: "/projects/astrojump/astrojump_1.png",
-                width: 4,
-                height: 3
-            }
-        ]
-    },
+    // {
+    //     name: "astrojump",
+    //     title: "AstroJump",
+    //     subtitle: "",
+    //     desc: "",
+    //     tags: ["Programming", "Game development"],
+    //     images: [
+    //         {
+    //             src: "/projects/astrojump/astrojump_1.png",
+    //             width: 4,
+    //             height: 3
+    //         }
+    //     ]
+    // },
     {
         name: "o3s",
         title: "O3S",
         subtitle: "Product design for the fictional instant oatmeal brand Oats, Oats, Oatmeals.",
         desc: "A graphic design project where a product design was developed for the brand Oats, Oats, Oatmeals, offering four different variants of instant oatmeal with one unified product. This included the design of a logo, business cards, a graphic profile, and examples of food packaging. The graphic material encompassed typography, color schemes, and graphic elements. All materials were created in Adobe Illustrator.",
         tags: ["Graphic design"],
-        tools: ["Adobe"],
+        tools: ["adobe"], 
         images: [
             {
                 src: "/projects/o3s/o3s_1.png",
@@ -194,6 +202,62 @@ export const projects = [
                 width: 4,
                 height: 4
             }
+        ]
+    },
+    {
+        name: "florilegium",
+        title: "Florilegium",
+        subtitle: "Digital prototype for the fictional book publishing company Florilegium. ",
+        desc: "",
+        tags: ["Graphic design"],
+        tools: ["adobe"],
+        demo: "https://xd.adobe.com/view/d0e74c17-7986-4131-9b72-59496cff570c-85b4/?fullscreen",
+        images: [
+            {
+                src: "/projects/florilegium/florilegium_1.png",
+                width: 11,
+                height: 6
+            },
+            {
+                src: "/projects/florilegium/florilegium_2.png",
+                width: 11,
+                height: 6
+            },
+            {
+                src: "/projects/florilegium/florilegium_3.png",
+                width: 11,
+                height: 6
+            },
+            {
+                src: "/projects/florilegium/florilegium_4.png",
+                width: 11,
+                height: 6
+            },
+            {
+                src: "/projects/florilegium/florilegium_5.png",
+                width: 11,
+                height: 6
+            },
+            {
+                src: "/projects/florilegium/florilegium_6.png",
+                width: 11,
+                height: 6
+            },
+            {
+                src: "/projects/florilegium/florilegium_7.png",
+                width: 11,
+                height: 6
+            },
+            {
+                src: "/projects/florilegium/florilegium_8.png",
+                width: 11,
+                height: 6
+            },
+            {
+                src: "/projects/florilegium/florilegium_9.png",
+                width: 11,
+                height: 6
+            },
         ]
     },
 ]
